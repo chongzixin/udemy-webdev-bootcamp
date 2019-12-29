bdb1e50d5f5da4221968f92a6fb7be2ca6dc88a8
chongzixin <zixin@chope.co>
2019-12-29 20:03:09 +0800

    finished section-30

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true, useUnifiedTopology: true});
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
@@ -24,13 +25,13 @@ app.get("/campgrounds", (req, res) => {
     Campground.find({}, (err, campgrounds) => {
         if(err) console.log(err);
         else {
-            res.render("campground", { campgrounds: campgrounds });
+            res.render("index", { campgrounds: campgrounds });
         }
     });
 });
 
 app.post("/campgrounds", (req, res) => {
-    var newCampground = {name: req.body.name, image: req.body.imgURL};
+    var newCampground = {name: req.body.name, image: req.body.imgURL, description: req.body.description};
     // insert campground to DB
     Campground.create(newCampground, (err, campground) => {
         if(err) {
@@ -45,6 +46,16 @@ app.post("/campgrounds", (req, res) => {
 
 app.get("/campgrounds/new", (req, res) => {
     res.render("new.ejs");
+});
+
+// CRITICAL TO PUT /NEW BEFORE /:id OTHERWISE /NEW WILL NEVER BE PROCESSED
+app.get("/campgrounds/:id", (req, res) => {
+    var id = req.params.id;
+    // retrieve the ID from the db
+    Campground.findById(id, (err, campground) => {
+        if(err) console.log(err)
+        else res.render("show", {campground: campground});
+    });
 });
 
 app.listen(3000, () => {
