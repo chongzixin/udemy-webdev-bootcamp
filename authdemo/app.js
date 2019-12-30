575716f1f7547f3b978415b24b50095ff86350a3
chongzixin <zixin@chope.co>
2019-12-30 18:41:09 +0800

    create user model and configure passport

--- authdemo/app.js
+++ authdemo/app.js
@@ -1,10 +1,26 @@
 var express = require("express"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    localStrategy = require("passport-local"),
+    expressSession = require("express-session"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo_app", {useUnifiedTopology:true, useNewUrlParser:true});
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressSession({
+    secret: "Thur likes to eat bread",
+    resave: false,
+    saveUninitialized: false
+}));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render("home");
