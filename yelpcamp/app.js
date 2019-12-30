be4db643dc63bdbe3c7163121ffc958f3c3e76f2
chongzixin <3539074+chongzixin@users.noreply.github.com>
2019-12-30 20:59:32 +0800

    Section 35 (#15)
    * added packages and define user model
    
    * added register, login and logout function
    
    * middleware to block ability to post comments if not logged in
    
    * changed navbar to reflect based on logged in user

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,14 +1,37 @@
 var express = require("express"),
+    ExpressSession = require("express-session"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
-    Comment = require("./models/comment")
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds.js");
 
 var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// PASSPORT CONFIG
 app.use(express.static(__dirname + "/public"));
+app.use(ExpressSession({
+    secret: "Thur likes to read",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// this is a middleware that will send req.user to every route (res)
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 seedDB();
 
 // MONGODB
@@ -63,7 +86,7 @@ app.get("/campgrounds/:id", (req, res) => {
 // =================
 
 // form to add new comments
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
     var id = req.params.id;
     Campground.findById(id, (err, campground) => {
         if(err) console.log(err);
@@ -73,7 +96,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
     });
 });
 
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
     var id = req.params.id;
     var comment = req.body.comment;
     
@@ -91,6 +114,54 @@ app.post("/campgrounds/:id/comments", (req, res) => {
         }
     });
 });
+
+// =================
+// AUTH ROUTES
+// =================
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var newUser = new User({ username: username });
+
+    User.register(newUser, password, (err, user) => {
+        if(err) {
+            console.log(err);
+            return res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/campgrounds");
+            });
+        }
+    });
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), (req, res) => {
+
+})
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, () => {
     console.log("server started running...");
